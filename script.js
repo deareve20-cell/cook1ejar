0a51230199dfd1947ec918816121f202644a10b7
M-PINK-L <deareve20@gmail.com>
2026-03-18 14:59:31 +0900

    .
    .

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-    $(".main-container").addClass("show");
-})
+$(document).ready(function () {
+    $('.main-container').addClass('show');
+});
 
-$(document).on('click', '.center-img-area', function(e) {
+$(document).on('click', '.center-img-area', function (e) {
     e.preventDefault();
 
-    $(".main-container").removeClass("show");
+    $('.main-container').removeClass('show');
 
-    $(".sub-container").addClass("show");
-})
+    $('.sub-container').addClass('show');
+});
 
-$(document).on('click', '.btn', function(e) {
+$(document).on('click', '.btn', function (e) {
     e.preventDefault();
-    $(".sub-center-img-area").children().remove();
+    $('.sub-center-img-area').children().remove();
 
-    const id = $(this).attr("id");
+    const id = $(this).attr('id');
     console.log(id);
     const img = $('<img>').attr('src', `./assets/260317/orgImg/${id}.png`);
-    $(".sub-center-img-area").append(img);
-})
+    $('.sub-center-img-area').append(img);
+});
